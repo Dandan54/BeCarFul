218364cd02b3ac79f65e46a85cc71ff21ed9bdb4
Mchl37 <71225509+Mchl37@users.noreply.github.com>
2021-01-12 08:53:26 +0100

    Ajout permis1euro.php + texte presentation

--- mvc_poo/home.php
+++ mvc_poo/home.php
@@ -5,6 +5,18 @@
     </div>
 </div>
 
+<!-- <div class="corps2">
+    <p>L'auto-école BeCarFul, à Paris, propose ses formations Auto (B, AAC, Supervisée, Post-permis, ...) avec un seul but : la réussite et la sécurité de ses élèves.</p>
+    <p>Chaque formation est dispensée par un enseignant diplômé, expérimenté et sérieux.</p>
+    <p>Votre vie d'automobiliste commence par un apprentissage évalué au début afin d'estimer le nombre de leçons de conduite dont vous seriez susceptible d'avoir besoin.</p>
+    <p>L'objectif principal de Maxi Permis est de dispenser une formation de qualité et sans surprise tout en mettant le permis à la portée du plus grand nombre.<br>
+    Ainsi, dans nos agences auto-école à Fourmies et Sars-Poteries, les informations sont claires, un maximum de prestations sont incluses ou offertes dans les forfaits, garantissant un tarif compétitif, sans surcoûts donc sans mauvaises surprises. De plus, une garantie financière est incluse dans tous nos forfaits.</p>
+    <p>L'auto-école Maxi Permis met à votre disposition deux véhicules équipés pour les leçons de conduite , deux motos et un scooter .<br>
+    Pour l'élève en leçon de permis moto ou AM (BSR) , l'auto-école est équipée de liaison radio (pour phase de formation en circulation publique).<br>
+    Maxi Permis possède ses propres véhicules</p>
+</div> -->
+
+
 <footer>
     © 2020 - TOUS DROITS RÉSERVÉS
 </footer>
--- mvc_poo/permis1euro.php
+++ mvc_poo/permis1euro.php
@@ -1,10 +1,54 @@
-<h2>Le permis à 1€ par jour, c’est quoi ?</h2>
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-<p>Le permis à 1€ par jour est un dispositif mis en place par l'État, en partenariat avec les établissements prêteurs et les écoles de conduite*, pour aider les jeunes à financer leur formation au permis de conduire.</p>
-<p>L’objectif : favoriser l’accès au permis de conduire et l'insertion professionnelle des jeunes.</p>
-<p>Il est destiné à tous les jeunes de 15 à 25 ans qui veulent s'inscrire à une formation au permis de conduire voiture (permis B) ou au permis de conduire moto (permis A1 et A2). Il est bien entendu accessible aux jeunes qui souhaitent bénéficier des formations de conduites accompagnées (conduite anticipée et conduite supervisée).</p>
-<p>Le permis à 1€ par jour permet d'étaler le coût de la formation sur plusieurs mois, par l'octroi d'un prêt à taux zéro. Le crédit accordé au jeune ne comprend aucun frais de dossier et l'Etat prend en charge les intérêts.</p>
+    <title>BeCarFul</title>
+    
+    <link rel="stylesheet" href="style.css">
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Roboto&display=swap" rel="stylesheet">
+</head>
+<body>
+    <div class="menu">
+        <h1>BeCarFul</h1>
+
+        <ul class="menu-1">
+            <li>
+                <a class="boutonmenu" href="index.php?page=0">Accueil</a>
+            </li>
+            <li>
+                <a class="boutonmenu" href="index.php?page=1">Offres</a>
+            </li>
+        </ul>
+        <ul class="menu-2">
+            <li>
+                <a class="boutonmenu" href="index.php?page=2">Contact</a>
+            </li>
+            <li>
+                <a class="button1" href="index.php?page=3">Connexion</a>
+            </li>
+        </ul>
+    </div>
+
+    <hr>
+
+    <h2>Le permis à 1€ par jour, c’est quoi ?</h2>
+
+    <div class="permis1e" style="width:50%; margin: 80px auto 50px auto; text-align: justify;">
+        <p>Le permis à 1€ par jour est un dispositif mis en place par l'État, en partenariat avec les établissements prêteurs et les écoles de conduite*, pour aider les jeunes à financer leur formation au permis de conduire.</p>
+        <br>
+        <p>L’objectif : favoriser l’accès au permis de conduire et l'insertion professionnelle des jeunes.</p>
+        <br>
+        <p>Il est destiné à tous les jeunes de 15 à 25 ans qui veulent s'inscrire à une formation au permis de conduire voiture (permis B) ou au permis de conduire moto (permis A1 et A2). Il est bien entendu accessible aux jeunes qui souhaitent bénéficier des formations de conduites accompagnées (conduite anticipée et conduite supervisée).</p>
+        <br>
+        <p>Le permis à 1€ par jour permet d'étaler le coût de la formation sur plusieurs mois, par l'octroi d'un prêt à taux zéro. Le crédit accordé au jeune ne comprend aucun frais de dossier et l'Etat prend en charge les intérêts.</p>
+    </div>
 
 <footer>
     © 2020 - TOUS DROITS RÉSERVÉS
 </footer>
+
+</body>
+</html>
